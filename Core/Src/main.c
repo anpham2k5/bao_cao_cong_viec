42bce64fe142286d0ae3e981543516cb1858e486
anpham2k5 <anphamx11pck@gmail.com>
2026-04-09 10:03:10 +0700

    congviec

---
+++ Core/Src/main.c
@@ -0,0 +1,239 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <string.h>
+
+TIM_HandleTypeDef htim2;
+UART_HandleTypeDef huart1;
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART1_UART_Init(void);
+
+float Measure_RC_Once_US(void);
+void PA1_As_Output_Low(void);
+void PA1_As_Input(void);
+void UART_Send(char *s);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART1_UART_Init();
+
+  HAL_TIM_Base_Start(&htim2);
+
+  while (1)
+  {
+    float rc;
+    char msg[50];
+
+    rc = Measure_RC_Once_US();
+
+    sprintf(msg, "RC_us = %.2f\r\n", rc);
+    UART_Send(msg);
+
+    HAL_Delay(1000);
+  }
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;   // 72 MHz -> 1 tick = 1/72 us
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 65535;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+void PA1_As_Output_Low(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+}
+
+void PA1_As_Input(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+float Measure_RC_Once_US(void)
+{
+  uint32_t timeout;
+  uint32_t ticks;
+
+  
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+  PA1_As_Output_Low();
+  HAL_Delay(1);
+
+ 
+  PA1_As_Input();
+
+  __HAL_TIM_SET_COUNTER(&htim2, 0);
+
+  
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+
+  timeout = 65000;
+
+  while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) == GPIO_PIN_RESET)
+  {
+    if (__HAL_TIM_GET_COUNTER(&htim2) > timeout)
+    {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+      ticks = timeout;
+      return (float)ticks / 72.0f;
+    }
+  }
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  ticks = __HAL_TIM_GET_COUNTER(&htim2);
+
+  
+  return (float)ticks / 72.0f;
+}
+
+void UART_Send(char *s)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t*)s, strlen(s), 100);
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif
---
+++ README.md
@@ -0,0 +1,99 @@
+# Báo cáo tiến độ nghiên cứu ngày 27/03/2026
+## A. Công việc đã làm
+- Nghiên cứu cảm biến xúc giác điện dung
+- Thực hành các thí nghiệm đo dữ liệu cảm biến
+- Nghiên cứu cảm biến xúc giác từ tính
+## B. Khó Khăn
+- Tính lực bằng neural network
+## C. Báo cáo chi tiết
+### 1. Nghiên cứu cảm biến xúc giác điện dung
+#### a. Cấu trúc phần cứng
+- 2 miếng PCB đặt song song như 2 bản tụ và 1 lớp điện môi đàn hồi.
+- Lớp điện môi đàn hồi, dễ nén khi chịu lực, cho phép khoảng cách giữa hai bản cực thay đổi theo lực tác động.
+#### b. Nguyên lí điện dung
+- Khi có lực tác động, lớp điện môi đàn hồi sẽ biến dạng làm cho 2 bản tụ gần nhau hơn khiến điện dung tăng. 
+$$
+C = \frac{\varepsilon S}{d}
+$$
+#### c. Thực hành thí nghiệm đo dữ liệu cảm biến điện dung
+- Phương pháp đo được sử dụng là đo thời gian nạp của mạch RC. Cảm biến điện dung được xem như một tụ điện biến thiên. STM32 trước tiên xả tụ, sau đó cho tụ nạp qua một điện trở cố định và dùng timer để đo khoảng thời gian từ lúc bắt đầu nạp đến khi điện áp trên tụ đạt ngưỡng logic HIGH của chân vào. Do thời gian nạp tỉ lệ với tích RC, khi lực tác động làm điện dung thay đổi thì thời gian đo được cũng thay đổi tương ứng. Từ giá trị thời gian này có thể đánh giá mức biến thiên điện dung của cảm biến.
+
+<div align="center">
+
+![](image/hinh10.jpg)
+
+</div>
+
+##### Linh kiện sử dụng:
+- Stm32f103c8t6
+- Trở : 2M
+
+#### d. Thí nghiệm đo điện dung C với các vật liệu và kích thước khác nhau
+Thông số thí nghiệm: https://docs.google.com/spreadsheets/d/1oDMeJInywCcWdUgZgRkDI_ojWt-54ljqLPaGA13Ekbc/edit?gid=211544715#gid=211544715
+
+#### e. Cách tính lực của cảm biến
+##### Thu thập dữ liệu
+- Tác động các mức lực chuẩn khác nhau lên cảm biến.
+- Đo và ghi lại giá trị điện dung tương ứng của cảm biến.
+- Tạo tập dữ liệu gồm các cặp giá trị (điện dung C, lực F).
+##### Xây dựng dữ liệu huấn luyện
+- Điện dung đo được từ cảm biến được sử dụng làm đầu vào (input) của mạng nơ-ron.
+- Lực tác động thực tế được sử dụng làm đầu ra (output) của mạng.
+##### Thiết kế mô hình mạng nơ-ron
+Sử dụng mạng nơ-ron truyền thẳng nhiều lớp (MLP).
+Cấu trúc gồm:
+- lớp đầu vào: giá trị điện dung hoặc tần số đo được
+- lớp ẩn: học mối quan hệ phi tuyến
+- lớp đầu ra: giá trị lực dự đoán.
+##### Huấn luyện mạng nơ-ron
+- Dữ liệu thu thập được được dùng để huấn luyện mạng.
+- Mạng điều chỉnh các trọng số để giảm sai số giữa lực dự đoán và lực thực tế.
+##### Sử dụng mô hình để dự đoán lực
+- Khi hệ thống hoạt động, điện dung đo được từ cảm biến được đưa vào mạng nơ-ron.
+- Mạng nơ-ron sẽ tính toán và xuất ra giá trị lực tương ứng.
+### 2. Nghiên cứu cảm biến xúc giác từ tính
+#### a. Cấu trúc phần cứng
+- Lớp vỏ cảm nhận cơ học: Sử dụng mạng lưới vi cấu trúc (cut-cell microstructures) kết hợp công cụ mã nguồn mở, cho phép tùy biến linh hoạt kích thước ô lưới, độ dày dầm và cấu hình phân tầng độ cứng.
+<div align="center">
+
+![](image/hinh6.png)
+
+</div>
+
+- Hệ thống phát tín hiệu từ tính: Gồm các nam châm được đặt trực tiếp vào các túi chứa bên trong lưới. Các nam châm này được sắp xếp phân cực xen kẽ nhằm triệt tiêu nhiễu từ trường.
+<div align="center">
+
+![](image/hinh7.png)
+
+</div>
+Link tham khảo cấu trúc vi mô phần cứng:https://github.com/notvenky/eFlesh/tree/main/microstructure
+
+- Hệ thống thu thập tín hiệu : Tích hợp bảng mạch từ kế chứa các cảm biến Hall, được đặt trong một khe cắm chuyên dụng ở phần đế để trực tiếp đo lường sự biến thiên từ trường.
+<div align="center">
+
+![](image/hinh8.png)
+
+</div>
+
+Link tham khảo cảm biến từ kế:https://github.com/raunaqbhirangi/reskin_sensor/tree/main/circuits
+Giá thành đặt khoảng 30 đô.
+
+#### b. Cơ chế tạo và thu nhận tín hiệu từ tính
+- Nguyên lý: Khi có ngoại lực làm biến dạng bề mặt cảm biến, các nam châm bên trong sẽ bị xê dịch, kéo theo sự thay đổi của từ trường xung quanh.
+- Thu nhận: Sự biến thiên từ trường này được đo lường trực tiếp bởi các cảm biến hiệu ứng Hall được đặt bên trong hoặc bên dưới mạng lưới.
+- Tín hiệu đầu ra: Tín hiệu này được hệ thống xuất ra dưới dạng một vector 15 chiều(do có 5 cảm biến từ kế 3 trục) đại diện cho sự thay đổi của từ trường.
+<div align="center">
+
+![](image/hinh9.png)
+
+</div>
+
+#### c. Cách tính toán và suy ra lực
+- Bước 1: Chuyển đổi lực thành sự nhiễu loạn từ tính: Khi có lực tác động lên bề mặt, lớp vi cấu trúc nhựa in 3D sẽ bị lún hoặc lệch, kéo theo sự xê dịch vị trí của các nam châm bên trong. Sự xê dịch vật lý này trực tiếp làm thay đổi từ trường xung quanh
+- Bước 2: Số hóa thành tín hiệu 15 chiều: Bảng mạch từ kế chứa 5 cảm biến hiệu ứng Hall đặt bên dưới sẽ lập tức đo lường sự biến thiên từ thông này theo 3 trục (X, Y, Z). Các giá trị này được gộp lại thành một dải dữ liệu thô, gọi là vector 15 chiều.
+- Bước 3: Dùng AI (mạng MLP) để tính lực: Thay vì đưa vào các phương trình phức tạp, vector 15 chiều này được nạp thẳng vào một mạng nơ-ron đa tầng (MLP) siêu nhẹ. Mạng này chỉ có 2 lớp ẩn, mỗi lớp chứa 128 nơ-ron và sử dụng hàm kích hoạt ReLU. Mạng MLP sẽ đóng vai trò tính toán, ánh xạ (mapping) trực tiếp sự thay đổi hỗn độn của từ trường thành các con số cụ thể về Lực pháp tuyến (lực ép vuông góc) và Lực cắt (lực trượt ngang) với đơn vị là Newton (N)
+
+Bài báo tham khảo:https://arxiv.org/pdf/2506.09994
+
+
+
